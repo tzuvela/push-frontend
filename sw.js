459d18768b1c2dfe09169336ef405fd059aeeee6
tzuvela <54351463+tzuvela@users.noreply.github.com>
2026-06-20 23:18:16 +0100

    Add icon

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
 self.addEventListener("push", event => {
     console.log("Push event received: ", event.data ? event.data.text() : "No payload");
     // const data = event.data.text()
-    const text = event.data ? event.data.text(): "No payload";
+    // const text = event.data ? event.data.text(): "No payload";
+    const data = event.data ? event.data.text() : "No payload";
+    const options = {
+        body: data,
+        icon: "/icon/icon.webp",
+        badge: "/icon/icon.webp"
+    }
+
     // self.registration.showNotification(data.title, {
     //     body: data.body
-    self.registration.showNotification("New Message", {body: text});
+    // self.registration.showNotification("New Message", {body: text});
+    event.waitUntil(
+        self.registration.showNotification("Push Demo", options)
+    )
 });
